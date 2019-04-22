968de382e8bb675bfaefe480134a557cc3294aee
myaosato <tetu60u@yahoo.co.jp>
2019-04-22 22:02:55 +0900

    lambda-fizzbuzz/mod: example of z-combinator

--- lambda-fizzbuzz/z-combinator.lisp
+++ lambda-fizzbuzz/z-combinator.lisp
@@ -2,8 +2,25 @@
   ((lambda (x) (funcall f (lambda (y) (funcall (funcall x x) y))))
    (lambda (x) (funcall f (lambda (y) (funcall (funcall x x) y))))))
 
-(defun fact (f)
-  (lambda (x) 
+(defun fact (x)
+  (if (= x 1)
+      1
+      (* x (fact (1- x)))))
+
+(defun %fact (f)
+  (lambda (x)
     (if (= x 1)
-        1
-        (* x (funcall f (1- x))))))
+      1
+      (* x (funcall f (1- x))))))
+
+(defun fib (x)
+  (cond ((= x 0) 0)
+        ((= x 1) 1)
+        (t (+ (fib (- x 1)) (fib (- x 2))))))
+
+(defun %fib (f)
+  (lambda (x)
+    (cond ((= x 0) 0)
+          ((= x 1) 1)
+          (t (+ (funcall f (- x 1)) (funcall f (- x 2)))))))
+
