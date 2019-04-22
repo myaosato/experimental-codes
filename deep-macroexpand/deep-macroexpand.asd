675afff074e0863e724243aa52e38eaa2ec273cf
myaosato <tetu60u@yahoo.co.jp>
2019-04-22 22:01:35 +0900

    deep-macroexpand/add: asd file and fix function name

---
+++ deep-macroexpand/deep-macroexpand.asd
@@ -0,0 +1,7 @@
+;;don't edit
+(defsystem "deep-macroexpand"
+  :author "MIYAO Satoaki"
+  :license "MIT"
+  :class :package-inferred-system
+  :components((:file "deep-macroexpand"))
+  :mailto "tetu60u@yahoo.co.jp")
--- deep-macroexpand/deep-macroexpand.lisp
+++ deep-macroexpand/deep-macroexpand.lisp
@@ -1,8 +1,13 @@
+(uiop/package:define-package :deep-macroexpand/deep-macroexpand
+                             (:nicknames :deep-macroexpand) (:use :cl)
+                             (:shadow) (:export :deep-macroexpand) (:intern))
+(in-package :deep-macroexpand/deep-macroexpand)
+;;don't edit above
 (defun deep-macroexpand (exp)
   (if (atom exp)
       (let ((eexp (macroexpand exp)))
         (if (equal eexp exp)
             exp
-            (expand eexp)))
+            (deep-macroexpand eexp)))
       (let ((eexp (macroexpand exp)))
-        (mapcar #'expand (if (eq (car eexp) 'function) (cadr eexp) eexp)))))
+        (mapcar #'deep-macroexpand (if (eq (car eexp) 'function) (cadr eexp) eexp)))))
